f74b84b0e8015e702244e780dbc5c40490417350
Mihaela Lazar <Mihaela Lazar>
2017-04-08 23:31:06 +0300

    Mihaela: MVC classes for estate entity

---
+++ domain/src/main/java/com/sgbd/EstateServiceImpl.java
@@ -0,0 +1,7 @@
+package com.sgbd;
+
+/**
+ * Created by mihae on 4/8/2017.
+ */
+public class EstateServiceImpl implements EstateService {
+}
---
+++ repository/src/main/java/com/sgbd/repository/EstateRepository.java
@@ -0,0 +1,7 @@
+package com.sgbd.repository;
+
+/**
+ * Created by mihae on 4/8/2017.
+ */
+public interface EstateRepository {
+}
