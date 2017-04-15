6a9d4e837379a5c51ada41275d9eeeaa172baac8
MihaelaLazar <Acaiberry94>
2017-04-15 22:47:35 +0300

    mihaela: remove system.out.println and clean code

--- src/main/java/com/sgbd/controller/EstateController.java
+++ src/main/java/com/sgbd/controller/EstateController.java
@@ -18,30 +18,25 @@ import java.util.List;
 @RestController
 public class EstateController {
 
-    public static final int NUMBER_OF_COLUMNS = 13;
+    private static final int NUMBER_OF_COLUMNS = 13;
 
     @RequestMapping(path = "/add/property", method = RequestMethod.GET)
     public ResponseEntity<String> redirectToAddProperty(Response response, Request request) {
         response.setContentType("text/html");
+
         try {
             response.sendRedirect("/addPropertyRaluca.html");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return new ResponseEntity<>("Redirect to add property", HttpStatus.OK);
     }
 
     @RequestMapping(path = "/add/property", headers = "Accept=application/json", method = RequestMethod.POST)
     @ResponseBody
     public ResponseEntity<String> getAddProperty(Response response, Request request, @RequestBody EstateDTO estateDTO) {
-        System.out.println("ADD PROPERTY");
-        System.out.println(estateDTO);
-//        String postDataCopy = "";
-//        for (int index = 1; index < postData.length() -1; index++) {
-//            postDataCopy += postData.charAt(index);
-//        }
-//        String [] fields = postDataCopy.split(",");
-//
+
         try {
             OracleCon.getOracleCon().addProperty(estateDTO);
         } catch (SQLException e) {
@@ -49,12 +44,12 @@ public class EstateController {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
         return new ResponseEntity<>("Added property", HttpStatus.OK);
     }
 
     @RequestMapping(path = "/paginate", method = RequestMethod.GET)
     public Object getPaginatedTable(Request request, Response response) {
-        System.out.println("Redirect in paginateTable");
         response.setContentType("text/html");
 
         try {
@@ -62,44 +57,34 @@ public class EstateController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return "";
     }
 
     @RequestMapping(path = "/api/paginate", method = RequestMethod.GET)
     public ResponseEntity<CitiesDTO> getPaginatedTableData(Request request, Response response) {
-        System.out.println("Redirect in paginateTable POST");
         response.setContentType("application/json");
+
         CitiesDTO estatesByCity = new CitiesDTO();
-        String Uri = request.getQueryString();
-        System.out.println(Uri);
-        System.out.println(request.getParameter("start"));
-        System.out.println(request.getParameter("columns[0][search][value]"));
-        System.out.println(request.getParameter("columns[1][search][value]"));
         String[] filters = new String[14];
         for (int index = 0; index < NUMBER_OF_COLUMNS; index++) {
             filters[index] = request.getParameter("columns[" + index + "][search][value]");
         }
         try {
             estatesByCity = OracleCon.getOracleCon().getEstates(Integer.parseInt(request.getParameter("start")), Integer.parseInt(request.getParameter("draw")), filters);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+
         return new ResponseEntity<>(estatesByCity, HttpStatus.OK);
     }
 
     @RequestMapping(path = "/paginate/filters", method = RequestMethod.POST)
     public ResponseEntity<List<Estate>> getPaginatedTableDataFilters(Request request, Response response, @RequestBody String postData) {
-        System.out.println("Redirect in paginateTable POST");
         response.setContentType("application/json");
+
         List<Estate> estates = new ArrayList<>(1000);
-        List<String> estatesCity = new ArrayList<>();
-        String postDataCopy = "";
-        for (int index = 1; index < postData.length() - 1; index++) {
-            postDataCopy += postData.charAt(index);
-        }
-        String[] filters = postDataCopy.split(",");
+
         return new ResponseEntity<>(estates, HttpStatus.OK);
     }
 
--- src/main/java/com/sgbd/controller/UserController.java
+++ src/main/java/com/sgbd/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.sgbd.controller;
 
+import com.sgbd.DTO.LoginDTO;
+import com.sgbd.DTO.SignUpDTO;
 import com.sgbd.OracleCon;
 import com.sgbd.UserService;
-import com.sgbd.dto.LoginDTO;
-import com.sgbd.dto.SignUpDTO;
+import com.sgbd.util.ContentType;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -25,7 +27,6 @@ public class UserController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public Object apasa(Request request, Response response) {
-        System.out.println("Redirect in homePage");
         response.setContentType("text/html");
 
         try {
@@ -37,58 +38,51 @@ public class UserController {
     }
 
     @RequestMapping(path = "/error", method = RequestMethod.GET)
-    public Object error(Response response) {
+    public Object error(Response response){
         try {
             response.sendRedirect("/error.html");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "";
+        return"";
     }
+
 
 
     @RequestMapping(path = "/create/user", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<String> addPerson(Request request, Response response, @RequestBody SignUpDTO user) {
-        System.out.println("ADD USER");
-        //System.out.println(request.getHttpFields());
-        System.out.println(user);
+    public  ResponseEntity<String>  addPerson(Request request,Response response, @RequestBody SignUpDTO user) {
+
         try {
             OracleCon.getOracleCon().addUser(user);
         } catch (SQLException e) {
-            return new ResponseEntity<>("DUPLICATE", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("DUPLICATE",HttpStatus.FORBIDDEN);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         response.setContentType(JSON.getContentType());
-        return new ResponseEntity<>("Added in database", HttpStatus.OK);
+        return new ResponseEntity<>("Added in database",HttpStatus.OK);
     }
 
     @RequestMapping(path = "/verify/user", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<String> validateUser(Response response, Request request, @RequestBody LoginDTO loginDTO) {
-        System.out.println("VALIDATE USER");
-        System.out.println(loginDTO.toString());
-        String[] emailAndPassword = new String[3];
+    public  ResponseEntity<String>  validateUser(Response response,Request request,@RequestBody LoginDTO loginDTO) {
+        String[] emailAndPassword;
+
         try {
-            emailAndPassword = OracleCon.getOracleCon().validateUser(loginDTO);
+            emailAndPassword =  OracleCon.getOracleCon().validateUser(loginDTO);
         } catch (SQLException e) {
-            return new ResponseEntity<>("INVALID USER/PASSWORD", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("INVALID USER/PASSWORD",HttpStatus.FORBIDDEN);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            return new ResponseEntity<>("THERE WAS A INTERNAL PROBLEM. PLEASE TRY AGAIN LATER !",HttpStatus.INTERNAL_SERVER_ERROR);
         }
         HttpSession session = request.getSession();
         session.setAttribute("Username", emailAndPassword[0]);
         session.setAttribute("Password", emailAndPassword[1]);
 
-//        Cookie []cookie = request.getCookies();
-//        System.out.println("Cookies: " + cookie[0].getName());
-        System.out.println(session.getCreationTime() + " " + session.getAttribute("Username"));
-        if (session.getAttribute("Password") == null) {
-            System.out.println("attribute non-existent");
-        }
         return new ResponseEntity<>("Login succeed", HttpStatus.OK);
     }
+
 
 
 }
