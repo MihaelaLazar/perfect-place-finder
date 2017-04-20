209e5d9173038852a41018c7af7917a69effa037
MihaelaLazar <Acaiberry94>
2017-04-20 22:59:31 +0300

    Mihaela: input for search city

--- src/main/webapp/profile.js
+++ src/main/webapp/profile.js
@@ -57,3 +57,15 @@ $(function(){
                   }
           });
 });
+
+var currentDiv;
+for (i = 0; i < estates.length; i ++) {
+    currentDiv = $("<div id='"+ estates[i].id +"' class='only row'><div class='column'><div class='ui raised card' style='width:91%;margin-top:1%;margin-left:2%;'><div class='content'> <img class='right floated tiny ui image' src='" + estates[i].photo+"/profile.jpg' style='width:120px;'><div class='header'>" + estates[i].name + "</div><div class='meta'>" + estates[i].city + "</div><div class='description'>" + estates[i].description + "</div></div><div class='extra content'><div class='ui two buttons'><input type='hidden' name='estate' value='" + estates[i].id + "' /><button class='ui blue button' onclick='updateEstate(" + estates[i].id + ")'>Update</button><form method='get' action='estateDetails.html?estate=" + estates[i].id  +"' ><button class='ui basic black button' type='submit'>Delete</button></form></div></div></div></div></div>");
+    $("#estatesProfile").append(currentDiv);
+}
+
+function updateEstate(id) {
+    $('#updateAnnouncement').css("display", "block");
+    $('#announcements').css("display", "none");
+
+}
