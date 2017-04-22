697f94f0eb1f740cfaa460cf7f0589ca9b806bde
AcasandreiBeatrice <beatrice.raluca.br@gmail.com>
2017-04-22 18:03:53 +0300

    Raluca: EstateDetails with Log in and Sign up

--- src/main/webapp/estateDetails.js
+++ src/main/webapp/estateDetails.js
@@ -177,3 +177,200 @@ $('#propertiesA').click(function(){
             }, 500);
             return false;
         });
+
+        var modalSignUpEstateDetails = document.getElementById('signUpEstateDetails');
+        // When the user clicks anywhere outside of the modal, close it
+        var modalLoginEstateDetails = document.getElementById('logInEstateDetails');
+        var modalLoginStatusEstateDetails = document.getElementById('logInStatus-EstateDetails');
+        var modalLoginStatusFailedEstateDetails = document.getElementById('logInStatusFailed-EstateDetails');
+        window.onclick = function(event) {
+            if(event.target === modalSignUpEstateDetails )
+            {
+                modalSignUpEstateDetails.style.display = "none";
+                $('#first-name-input-EstateDetails').removeClass('error');
+                $('#first-name-EstateDetails').attr("placeholder","First name");
+                $('#last-name-input-EstateDetails').removeClass('error');
+                $('#last-name-EstateDetails').attr("placeholder","Last name");
+                $('#email-input-EstateDetails').removeClass('error');
+                $('#email-EstateDetails').attr("placeholder","Email");
+                $('#password-input-EstateDetails').removeClass('error');
+                $('#password-name-EstateDetails').attr("placeholder","Password");
+                $('#errorMessageContainer-EstateDetails').css("display", "none");
+                $("#email-EstateDetails").val("");
+                $("#passwordSignUp-EstateDetails").val("");
+                $("#first-name-EstateDetails").val("");
+                $("#last-name-EstateDetails").val("");
+            }
+            else {
+              if (event.target == modalLoginEstateDetails)
+              {
+                modalLoginEstateDetails.style.display = "none";
+                $('#email-input-login-EstateDetails').removeClass('error');
+                $('#email-login-EstateDetails').attr("placeholder","Email");
+                $('#password-input-login-EstateDetails').removeClass('error');
+                $('#errorMessageContainer-EstateDetails').css("display", "none");
+                $("#email-login-EstateDetails").val("");
+                $("#password-login-EstateDetails").val("");
+                $('#errorMessageContainer-login-EstateDetails').css("display", "none");
+              }
+              else
+              {
+                    if (event.target === document.getElementById('signInStatus-EstateDetails')) {
+                         document.getElementById('signInStatus-EstateDetails').style.display='none';
+                    } else {
+                        if (event.target ===  document.getElementById('signInStatusFailed-EstateDetails')){
+                            document.getElementById('signInStatusFailed-EstateDetails').style.display='none';
+                        } else {
+                            if (event.target === modalLoginStatusEstateDetails)  {
+                                document.getElementById('logInStatus-EstateDetails').style.display='none';
+                            } else {
+                              if (event.target === modalLoginStatusFailedEstateDetails) {
+                                 document.getElementById('logInStatusFailed-EstateDetails').style.display='none';
+                              }
+                            }
+                        }
+                    }
+              }
+            }
+        }
+
+        function logInPOSTEstateDetails() {
+                      $('#password-input-login-EstateDetails').removeClass('error');
+                      $('#email-input-login-EstateDetails').removeClass('error');
+                      console.log('logInPOST');
+                      var url = "/verify/user";
+                      var method = "POST";
+                      var passInput = document.getElementById('password-login-EstateDetails');
+                      var password = passInput.value;
+                      var emailInput = document.getElementById('email-login-EstateDetails');
+                      var email = emailInput.value;
+                      var isValid = true;
+                      var atpos = email.indexOf("@");
+                      var dotpos = email.lastIndexOf(".");
+                      isValid = true;
+                      if (password === "") {
+                            $('#password-input-login-EstateDetails').addClass('error');
+                            $('#password-login-EstateDetails').attr("placeholder","Insert password");
+                                 $('#errorMessageContainer-login-EstateDetails').text("Insert password");
+                                 $('#errorMessageContainer-login-EstateDetails').css("display", "block");
+                            isValid = false;
+                      }
+                      if (email === "" || atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+                            $('#email-input-login-EstateDetails').addClass('error');
+                            $('#email-login-EstateDetails').attr("placeholder","Insert email");
+                                $('#errorMessage-login-EstateDetails').text("Incorrect email type");
+                                $('#errorMessageContainer-login-EstateDetails').css("display", "block");
+                            isValid = false;
+                      }
+                      var postData = {
+                           "password" : password,
+                           "email" : email
+                          };
+                      if (isValid === true ){
+                          var async = true;
+                          var request = new XMLHttpRequest();
+                          var status;
+                          var data;
+                          request.onload = function () {
+                                status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+                                data = request.responseText; // Returned data, e.g., an HTML document.
+                                if (data === 'INVALID USER/PASSWORD') {
+                                    console.log ("incorrect EMAIL");
+                                    document.getElementById('logInEstateDetails').style.display='none';
+                                    document.getElementById('logInStatusFailed-EstateDetails').style.display='block';
+                                } else {
+                                  document.getElementById('logInEstateDetails').style.display='none';
+                                  document.getElementById('logInStatus-EstateDetails').style.display='block';
+                              }
+                          }
+                          request.open(method, url,true);
+                          request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                          request.send(JSON.stringify(postData));
+                          document.getElementById('logInEstateDetails').style.display='none';
+                          $("#email-login-EstateDetails").val("");
+                          $("#password-login-EstateDetails").val("");
+                          document.getElementById('errorMessage-login-EstateDetails').style.display = 'none';
+                      }
+
+               }
+
+        function signUpPOSTEstateDetails() {
+                      $('#password-input-EstateDetails').removeClass('error');
+                      $('#first-name-input-EstateDetails').removeClass('error');
+                      $('#last-name-input-EstateDetails').removeClass('error');
+                      $('#email-input-EstateDetails').removeClass('error');
+                      console.log('signUpPOST');
+                      var url = "/create/user";
+                      var method = "POST";
+                      var firstNameInput = document.getElementById('first-name-EstateDetails');
+                      var firstName = firstNameInput.value;
+                      var lastNameInput = document.getElementById('last-name-EstateDetails');
+                      var lastName = lastNameInput.value;
+                      var passInput = document.getElementById('passwordSignUp-EstateDetails');
+                      var password = passInput.value;
+                      var emailInput = document.getElementById('email-EstateDetails');
+                      var email = emailInput.value;
+                      var isValid = true;
+                      var atpos = email.indexOf("@");
+                      var dotpos = email.lastIndexOf(".");
+                     if (firstName === "") {
+                            $('#first-name-input-EstateDetails').addClass('error');
+                            $('#first-name-EstateDetails').attr("placeholder","Insert first name");
+                            $('#errorMessage-EstateDetails').text("Insert first name");
+                            $('#errorMessageContainer-EstateDetails').css("display", "block");
+                            isValid = false;
+                      }
+                      if (lastName === "") {
+                            $('#last-name-input-EstateDetails').addClass('error');
+                            $('#last-name-EstateDetails').attr("placeholder","Insert last name");
+                                $('#errorMessage-EstateDetails').text("Insert last name");
+                                $('#errorMessageContainer-EstateDetails').css("display", "block");
+                            isValid = false;
+                      }
+                      if (password === "") {
+                            $('#password-input-EstateDetails').addClass('error');
+                            $('#passwordSignUp-EstateDetails').attr("placeholder","Insert password");
+                                 $('#errorMessage-EstateDetails').text("Insert password");
+                                 $('#errorMessageContainer-EstateDetails').css("display", "block");
+                            isValid = false;
+                      }
+                      if (email === "" || atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+                            $('#email-input-EstateDetails').addClass('error');
+                            $('#email-EstateDetails').attr("placeholder","Insert email");
+                                $('#errorMessage-EstateDetails').text("Insert email");
+                                $('#errorMessageContainer-EstateDetails').css("display", "block");
+                            isValid = false;
+                      }
+                      var postData = {
+                          "firstName": firstName,
+                           "lastName" : lastName,
+                           "password" : password,
+                           "email" : email
+                          };
+                      if (isValid === true ){
+                          var async = true;
+                          var request = new XMLHttpRequest();
+                          var status;
+                          var data;
+                          request.onload = function () {
+                                status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+                                data = request.responseText; // Returned data, e.g., an HTML document.
+                                if (data === 'DUPLICATE') {
+                                    console.log ("DUPLICATE EMAIL");
+                                    document.getElementById('signUpEstateDetails').style.display='none';
+                                    document.getElementById('signInStatusFailed-EstateDetails').style.display='block';
+                                } else {
+                                  document.getElementById('signUpEstateDetails').style.display='none';
+                                  document.getElementById('signInStatus-EstateDetails').style.display='block';
+                              }
+                          }
+                          request.open(method, url,true);
+                          request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                          request.send(JSON.stringify(postData));
+                          document.getElementById('signUpEstateDetails').style.display='none';
+                           $("#email-EstateDetails").val("");
+                           $("#passwordSignUp-EstateDetails").val("");
+                           $("#first-name-EstateDetails").val("");
+                           $("#last-name-EstateDetails").val("");
+                      }
+               }
