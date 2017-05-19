7912f506fc3eba6efd157cf81bac789e5af87e32
MihaelaLazar <mihaela.lzr@gmail.com>
2017-05-19 07:55:28 +0300

    Mihaela: updated admin profile page

---
+++ src/main/webapp/adminProfile.js
@@ -0,0 +1,36 @@
+/* This function changes the appearance of every section when clicking button:
+    - update users section
+    - admin's account settings
+    - announcements
+    - messages
+*/
+function getFieldAdminProfile(id) {
+    $('#updateAnnouncement').css("display", "none");
+    if (id === 'users') {
+        $('#users').css("display", "block");
+        $('#accountSettings').css("display", "none");
+        $('#announcements').css("display", "none");
+        $('#messages').css("display", "none");
+    }
+
+    if (id === 'announcements') {
+        $('#users').css("display", "none");
+        $('#accountSettings').css("display", "none");
+        $('#announcements').css("display", "block");
+        $('#messages').css("display", "none");
+    }
+
+    if (id === 'settings') {
+        $('#users').css("display", "none");
+        $('#accountSettings').css("display", "block");
+        $('#announcements').css("display", "none");
+        $('#messages').css("display", "none");
+    }
+
+    if (id === 'messages') {
+        $('#users').css("display", "none");
+        $('#accountSettings').css("display", "none");
+        $('#announcements').css("display", "none");
+        $('#messages').css("display", "block");
+    }
+}
