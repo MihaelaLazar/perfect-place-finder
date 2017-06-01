a9b110a41ed98c167c5df072d898ed8e93faca7c
Rares <rares_sg@yahoo.com>
2017-06-01 11:07:05 +0300

    Added styling to the earthquake layer.

--- src/main/webapp/searchCity.js
+++ src/main/webapp/searchCity.js
@@ -1144,6 +1144,19 @@ function addOverlay(id) {
                                         'src',
                                         'https://storage.googleapis.com/mapsdevsite/json/quakes.geo.json');
                                     document.getElementsByTagName('head')[0].appendChild(script);
+
+                                    map.data.setStyle(function(feature) {
+                                        var mag = Math.exp(parseFloat(feature.getProperty('mag'))) * 0.1;
+                                        return /** @type {google.maps.Data.StyleOptions} */({
+                                            icon: {
+                                                path: google.maps.SymbolPath.CIRCLE,
+                                                scale: mag,
+                                                fillColor: '#f00',
+                                                fillOpacity: 0.35,
+                                                strokeWeight: 0
+                                            }
+                                        });
+                                    });
                                 }
                             }
                         }
